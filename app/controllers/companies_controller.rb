f9d1c2ce8d0624f34451c2f69a045b0546098e6c
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2012-11-27 16:26:02 +0200

    one style pages

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class CompaniesController < ApplicationController
   load_and_authorize_resource
 
@@ -28,7 +30,7 @@ class CompaniesController < ApplicationController
   def create
     respond_to do |format|
       if @company.save
-        format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.html { redirect_to @company, notice: 'Заказчик добавлен.' }
         format.json { render json: @company, status: :created, location: @company }
       else
         format.html { render action: "new" }
@@ -40,7 +42,7 @@ class CompaniesController < ApplicationController
   def update
     respond_to do |format|
       if @company.update_attributes(params[:company])
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to @company, notice: 'Заказчик изменен.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
