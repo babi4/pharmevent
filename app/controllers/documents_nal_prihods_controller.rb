d1c74e849950bae1d178bddc0ec24449559ef404
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2012-11-28 15:13:58 +0200

    event nal_prihods

--- app/controllers/documents_nal_prihods_controller.rb
+++ app/controllers/documents_nal_prihods_controller.rb
@@ -22,6 +22,7 @@ class DocumentsNalPrihodsController < ApplicationController
 
   def new
     @documents_nal_prihod = DocumentsNalPrihod.new
+    @documents_nal_prihod[:date] = DateTime.now.tomorrow.strftime("%d.%m.%Y")
 
     respond_to do |format|
       format.html # new.html.erb
