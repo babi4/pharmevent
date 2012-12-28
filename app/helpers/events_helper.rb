afef61822cb26ab2645ba314c6a8a24b34dde630
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2012-12-28 16:56:10 +0200

    color labels

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,17 +4,17 @@ module EventsHelper
   def event_state_text(state)
     case state
     when 'new'
-      'Новый'
+      content_tag 'span', 'Новый', class: 'label'
     when 'added'
-      'На подпись'
+      content_tag 'span', 'На подпись', class: 'label label-info'
     when 'signed'
-      'Подписан'
+      content_tag 'span', 'Подписан', class: 'label label-important'
     when 'for_revision'
-      'На доработку'
+      content_tag 'span', 'На доработку', class: 'label label-info'
     when 'paid'
-      'Оплачен'
+      content_tag 'span', 'Оплачен', class: 'label label-success'
     when 'not_for_payment'
-      'Запрещен к оплате'
+      content_tag 'span', 'Запрещен к оплате', class: 'label label-warning'
     end
   end
 end
