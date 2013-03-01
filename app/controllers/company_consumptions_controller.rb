595b1c0b6ddcc9f978bc4446f22add5e600264f3
Konstantin Zub <zubkonst@gmail.com>
2013-03-01 19:01:53 +0700

    fix

--- app/controllers/company_consumptions_controller.rb
+++ app/controllers/company_consumptions_controller.rb
@@ -4,7 +4,7 @@ class CompanyConsumptionsController < ApplicationController
   authorize_resource :class => false
 
   def index
-    @event = 0
+    @event = PseudoEvent.company_event
     @documents_nal_rashods = DocumentsNalRashod.by_company
     @documents_beznal_rashods = DocumentsBeznalRashod.by_company
     @rashod_summ = @documents_nal_rashods.sum(:summ) + @documents_beznal_rashods.sum(:summ)
